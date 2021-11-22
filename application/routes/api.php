b53aff3b950a8fa34607e5002a2bdb1fc0c548ce
9831065 <najafizadeh21@gmail.com>
2021-11-22 15:48:53 +0330

    API routes refactor

--- application/routes/api.php
+++ application/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,8 +19,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('user', [\App\Http\Controllers\API\UserController::class, 'create_user']);
+Route::post('user', [UserController::class, 'create_user']);
 
-Route::delete('user', [\App\Http\Controllers\API\UserController::class, 'remove_user']);
+Route::delete('user', [UserController::class, 'remove_user']);
 
-Route::post('notify', [\App\Http\Controllers\API\UserController::class, 'call_user']);
+Route::post('notify', [UserController::class, 'call_user']);
