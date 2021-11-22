f29c9787738fdecc12ed4264b134bbb081ca0008
9831065 <najafizadeh21@gmail.com>
2021-11-22 15:15:37 +0330

    User controller mehtods added:

--- application/app/Http/Controllers/API/UserController.php
+++ application/app/Http/Controllers/API/UserController.php
@@ -7,31 +7,73 @@ use App\Jobs\NotifyUser;
 use App\Models\Notification;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
+/**
+ * Class UserController handles the user features.
+ *
+ * @package App\Http\Controllers\API
+ */
 class UserController extends Controller
 {
     /**
-     * This method creates a notification for a user.
+     * Creating a user method.
      *
-     * @param User $user
-     * @param Notification $notification
+     * @param Request $request
      * @return JsonResponse
      */
-    private function call_user(User $user, Notification $notification): JsonResponse
+    public function create_user(Request $request): JsonResponse
     {
+        $user = User::query()
+            ->create($request->all());
+
+        return response()
+            ->json([
+                'user' => $user->id,
+                'status' => 'Success'
+            ]);
+    }
+
+    /**
+     * Method for removing a user.
+     *
+     * @param $id
+     * @return JsonResponse
+     */
+    public function remove_user($id): JsonResponse
+    {
+        User::query()
+            ->findOrFail($id)
+            ->delete();
+
+        return response()
+            ->json([
+                'status' => 'Deleted'
+            ]);
+    }
+
+    /**
+     * This method creates a notification for a user.
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    private function call_user(Request $request): JsonResponse
+    {
+        $user = User::query()
+            ->firstOrFail($request->get('user_id'));
+
+        $notification = Notification::query()
+            ->create($request->all());
+
         NotifyUser::dispatch(['user' => $user, 'notification' => $notification])
             ->delay(now()->addMinutes(5));
 
         return response()
             ->json([
                 'user' => $user->id,
-                'notify' => 'success'
+                'notify' => 'success',
+                'id' => $notification->id,
             ]);
     }
-
-    // TODO: Create user
-    // TODO: Delete user
-    // TODO: Create notify
-    // TODO: Assign notify to a user
-    // TODO: Remove the notify
 }
