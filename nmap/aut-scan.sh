1eb11559a8d1348642a5753c173f88365400d2d9
AmirH.Najafizadeh <najafizadeh21@gmail.com>
2023-04-10 19:03:25 +0330

    Update aut-scan.sh

--- nmap/aut-scan.sh
+++ nmap/aut-scan.sh
@@ -1,4 +1,5 @@
 #!/urs/bin/env bash
 
 # checkout all of the people who are connected to aut internet
-sudo nmap -sP -v 172.24.56.0/21
+# get the results in hosts.txt
+sudo nmap -sP -v -oN hosts.txt 172.24.56.0/21
